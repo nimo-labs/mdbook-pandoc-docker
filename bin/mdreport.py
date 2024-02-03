ba6d95838e839d8bce751470f09b0406f10dcc0e
Nick <nick@nimo.uk>
2024-02-03 10:26:38 +0000

    Added mdreport.py

---
+++ bin/mdreport.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+import subprocess
+import os
+
+intermediateFileName = "./output.inter.md"
+masterFileName = "src/report.md"
+outputFileName = "./output.pdf"
+templateFileName = "./src/template/template.latex"
+
+masterFile = open(masterFileName, mode = 'r', encoding = 'utf-8-sig')
+lines = masterFile.readlines()
+masterFile.close()
+
+content = ""
+
+for line in lines:
+    if '%' == line[0]:
+        srcFile = open("src/"+line[1:-1], 'r')
+        content = content + "\n\n" +srcFile.read()
+        srcFile.close()
+    else:
+        content = content + line
+destination_file = open(intermediateFileName, 'w')
+destination_file.write(content)
+destination_file.close()
+
+subprocess.run(["pandoc", "--listings", "--template", templateFileName, intermediateFileName, "-o", outputFileName])
+os.remove(intermediateFileName)
